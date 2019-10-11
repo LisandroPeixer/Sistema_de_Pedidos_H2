4a59639ed65c9fb0757ac6e08d71159df24e88b0
LisandroPeixer <lisandropeixer@yahoo.com.br>
2019-10-10 23:28:44 -0300

    Entidades e associações

--- src/main/java/com/example/demo/entities/Order.java
+++ src/main/java/com/example/demo/entities/Order.java
@@ -1,8 +1,9 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
-
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.demo.entities.enuns.OrderStatus;
@@ -32,6 +34,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> itens = new HashSet<>();
 	
 	public Order() {
 		
@@ -79,6 +84,11 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
+	public Set<OrderItem> getItens(){
+		return itens;
+	}
+	
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
